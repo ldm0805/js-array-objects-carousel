688008c874551d9d953e1fbfb7673358163ff1fc
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2022-12-14 14:23:30 +0100

    Container di oggetti creato

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let superhero = [
+    {
+    image: "./img/01.webp",
+    title: "Marvel's Spiderman Miles Morales",
+    description: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+    },
+    {
+    image: "./img/02.webp",
+    title: "Ratchet And Clank",
+    description: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+
+    },
+    {
+    image: "./img/03.webp",
+    title: "Fortnine",
+    description: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+
+    },
+    {
+    image: "./img/04.webp",
+    title: "Stray cat",
+    escription: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+
+    },
+    {
+    image: "./img/05.webp",
+    title: "Avengers",
+    description: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+    },
+]
+console.log(superhero)
